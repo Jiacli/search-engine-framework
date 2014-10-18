21c1b3a6c2e79641f25e042e1fb159f2e34b7369
Jiachen Li <jiacli.jk@gmail.com>
2014-10-17 22:17:54 -0400

    Add class for WAND, WSUM

---
+++ src/QryopSlWsum.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+
+
+public class QryopSlWsum extends QryopSl {
+
+    public QryopSlWsum() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public double getDefaultScore(RetrievalModel r, long docid)
+            throws IOException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void add(Qryop q) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public QryResult evaluate(RetrievalModel r) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
