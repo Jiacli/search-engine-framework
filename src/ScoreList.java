ffc8a1059f25a14840e67c3978d5817d1fb2e440
Jiachen Li <jiacli.jk@gmail.com>
2014-11-01 13:42:08 -0400

    Update some formats, delete last report

--- src/ScoreList.java
+++ src/ScoreList.java
@@ -9,60 +9,67 @@ import java.util.*;
 
 public class ScoreList {
 
-  //  A little utilty class to create a <docid, score> object.
+    // A little utilty class to create a <docid, score> object.
 
-  protected class ScoreListEntry {
-    private int docid;
-    private double score;
-    public String extId;
+    protected class ScoreListEntry {
+        private int docid;
+        private double score;
+        public String extId;
 
-    private ScoreListEntry(int docid, double score) {
-      this.docid = docid;
-      this.score = score;
-      this.extId = null;
+        private ScoreListEntry(int docid, double score) {
+            this.docid = docid;
+            this.score = score;
+            this.extId = null;
+        }
+
+        public int getDocId() {
+            return docid;
+        }
     }
-    
-    public int getDocId() {
-        return docid;
+
+    List<ScoreListEntry> scores = new ArrayList<ScoreListEntry>();
+
+    /**
+     * Append a document score to a score list.
+     * 
+     * @param docid
+     *            An internal document id.
+     * @param score
+     *            The document's score.
+     * @return void
+     */
+    public void add(int docid, double score) {
+        scores.add(new ScoreListEntry(docid, score));
     }
-  }
 
-  List<ScoreListEntry> scores = new ArrayList<ScoreListEntry>();
+    /**
+     * Get the n'th document id.
+     * 
+     * @param n
+     *            The index of the requested document.
+     * @return The internal document id.
+     */
+    public int getDocid(int n) {
+        return this.scores.get(n).docid;
+    }
 
-  /**
-   *  Append a document score to a score list.
-   *  @param docid An internal document id.
-   *  @param score The document's score.
-   *  @return void
-   */
-  public void add(int docid, double score) {
-    scores.add(new ScoreListEntry(docid, score));
-  }
+    /**
+     * Get the score of the n'th document.
+     * 
+     * @param n
+     *            The index of the requested document score.
+     * @return The document's score.
+     */
+    public double getDocidScore(int n) {
+        return this.scores.get(n).score;
+    }
 
-  /**
-   *  Get the n'th document id.
-   *  @param n The index of the requested document.
-   *  @return The internal document id.
-   */
-  public int getDocid(int n) {
-    return this.scores.get(n).docid;
-  }
-
-  /**
-   *  Get the score of the n'th document.
-   *  @param n The index of the requested document score.
-   *  @return The document's score.
-   */
-  public double getDocidScore(int n) {
-    return this.scores.get(n).score;
-  }
-  
-  public static int ScoreCompare(ScoreListEntry e1, ScoreListEntry e2) {
-      if (e2.score < e1.score)
-          return -1;
-      else if (e2.score > e1.score)
-          return 1;
-      else
-          return 0;
-  }
+    public static int ScoreCompare(ScoreListEntry e1, ScoreListEntry e2) {
+        if (e2.score < e1.score)
+            return -1;
+        else if (e2.score > e1.score)
+            return 1;
+        else
+            return 0;
+    }
 }
